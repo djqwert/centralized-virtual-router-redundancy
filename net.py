a4a5fb02a7ad9d28cfa450f5aa274fc9c2ac096f
djqwert <djqwert@hotmail.it>
2020-01-20 16:17:01 +0100

    Sono presenti il file di rete net.py e l'applicativo da installare sul router.py. E' possibile notare anche il file TODO.txt e il documento di progetto.

---
+++ net.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
+
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
+
+def myNetwork():
+
+    net = Mininet( topo=None, build=False, ipBase='10.0.0.0/24')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=RemoteController,
+                      ip='127.0.0.1',
+                      protocol='tcp',
+                      port=6653)
+
+    info( '*** Add routers\n')
+    r1 = net.addHost('r1', cls=LinuxRouter, ip='10.0.2.1/24', mac='00:00:00:00:00:01')
+    r2 = net.addHost('r2', cls=LinuxRouter, ip='10.0.2.2/24', mac='00:00:00:00:00:02')
+    
+    info( '*** Add switches\n')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)    
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch, failMode='standalone')
+
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.2.3/24')
+    h2 = net.addHost('h2', cls=Host, ip='10.0.2.4/24')
+    h3 = net.addHost('h3', cls=Host, ip='10.0.2.5/24')
+    h4 = net.addHost('h4', cls=Host, ip='10.0.3.3/24', defaultRoute='via 10.0.3.2')
+    h5 = net.addHost('h5', cls=Host, ip='10.0.3.4/24', defaultRoute='via 10.0.3.2')
+    h6 = net.addHost('h6', cls=Host, ip='10.0.3.5/24', defaultRoute='via 10.0.3.2')
+
+    info( '*** Add links\n')
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(h3, s1)
+    net.addLink(s1, r1, intfName2='eth0', params2={ 'ip' : '10.0.2.1/24' })
+    net.addLink(s1, r2, intfName2='eth0', params2={ 'ip' : '10.0.2.2/24' })
+    net.addLink(s2, r1, intfName2='eth1', params2={ 'ip' : '10.0.3.1/24' })
+    net.addLink(s2, r2, intfName2='eth1', params2={ 'ip' : '10.0.3.2/24' })
+    net.addLink(s2, h4)
+    net.addLink(s2, h5)
+    net.addLink(s2, h6)
+
+    info( '*** Starting network\n')
+    net.build()
+    
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s2').start([])
+    net.get('s1').start([c0])
+
+    info( '*** Post configure switches and hosts\n')
+    #net.get('r1').cmd('python ./router.py &')
+    #net.get('r2').cmd('python ./router.py &')
+
+    CLI(net)
+    
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
---
+++ router.py
@@ -0,0 +1,95 @@
+# This protocol to work correctly need a Floodlight controller
+# installed and already actived inside the network
+
+from random import random
+import time
+import socket  
+import select
+import netifaces as ni
+
+''' Set of parameters '''
+INTERFACE_IP = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+BROADCAST_ADDRESS = "10.0.2.255"
+COMM_PORT = 12345
+ROUTER_STATE = 0        # 0: no-state, 1: backup, 2: master
+WAITING_TIME = 3        # in real cases WAITING_TIME must be about 1 ms (0.001)
+VRID = int(round(1 + (random() * (255 - 1))))
+
+''' Start configuration '''
+print "[INFO]"
+print "eth0: "
+print "\tinet " + INTERFACE_IP + " netmask 255.255.255.0 " + "broadcast " + BROADCAST_ADDRESS
+print "VRID: " + str(VRID)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+print "[INFO] Socket created"
+
+sock.bind(("", COMM_PORT))
+
+print "[INFO] Socket bound to port %d" %(COMM_PORT)
+
+sock.sendto(str(VRID), (BROADCAST_ADDRESS, COMM_PORT))
+print "[INFO] Packet sent to %s throught port %s" %(BROADCAST_ADDRESS, COMM_PORT)
+
+''' Router selection '''
+try:
+    sock.settimeout(60);
+    while True:
+        data, addr = sock.recvfrom(1024)
+        if addr[0] != INTERFACE_IP:
+            break;
+except:
+    print "[WARN] VRRP interrupted because no packet was received until the timeout"
+    sock.close()
+    exit()
+
+sock.settimeout(None)
+
+data = int(data);
+
+print "I received a VRID = ", data
+
+if VRID < data:
+    ROUTER_STATE = 1
+else:
+    ROUTER_STATE = 2
+    
+''' Queue must be empty '''
+try:
+    sock.setblocking(0)	    # non-blocking mode actived
+    while True:
+        data, addr = sock.recvfrom(1024)
+except:
+    print "[INFO] Receive queue has been emptied"
+    
+sock.setblocking(1)	
+
+''' Main program '''
+while True:
+    
+    if ROUTER_STATE == 1:                       # I am waiting for my colleague router goes down
+        print "[INFO] I am the virtual backup router"
+        data, addr = sock.recvfrom(1024) 
+        data = int(data)
+        if VRID > data:
+            ROUTER_STATE == 2
+        continue;
+        
+    if ROUTER_STATE == 2:                       # I am the master! Yeah!
+        print "[INFO] I am the virtual master router"
+        while True:
+        
+            sock.sendto(str(VRID), (BROADCAST_ADDRESS, COMM_PORT));
+            print "[INFO] VRRP advertisement sent to (%s, %d)" %(BROADCAST_ADDRESS, COMM_PORT)
+            time.sleep(WAITING_TIME)
+        
+    else:                                       # OMG, What did it happen here?
+    
+        print "[ERR] Something went wrong during the selection role"
+        sock.close()
+        exit();
+
+sock.close()
